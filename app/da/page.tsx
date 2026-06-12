eb478027286809b9f5e8acb21783ac82800122e6
mikkelstreet-png <mikkelstreet@outlook.dk>
2026-06-12 11:45:16 +0200

    Use premium Naetwork experience on Danish page

--- app/da/page.tsx
+++ app/da/page.tsx
@@ -1,5 +1,5 @@
-import { NaetworkLaunchMvp } from "@/components/NaetworkLaunchMvp";
+import { NaetworkExperience } from "@/components/NaetworkExperience";
 
 export default function HomeDa() {
-  return <NaetworkLaunchMvp />;
+  return <NaetworkExperience />;
 }
